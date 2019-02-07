f6af7568d195b022a85e3c189f852771a1c7b6bf
Johan Heidenberg <jheidenberg@outlook.com>
2019-02-07 22:29:00 +0100

    refactoring

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,13 +1,9 @@
 
- 
 class Bubblesort
-  
-  attr_accessor :is_sorted, :counter
 
   def initialize
-    @shifted = ""
+    @num_of_iterations = 0
   end
-
 
   def sort_status
     @is_sorted = false
@@ -17,38 +13,31 @@ class Bubblesort
     @arr = arr
     i = 0
     temp = 0
-
    (arr.length - 1).times do
-  
-      orig_array = 
-    # store array in a var
-
-      while i < (arr.length - 1) do
-
-        if arr[i] > arr[i + 1]
-          temp = arr[i]
+      @num_shifts = 0
+        while i < (arr.length - 1) do
+        if arr[i] > arr[i + 1] then temp = arr[i]
           arr[i] = arr[i + 1]
           arr[i + 1] = temp
-        end
-
-        i += 1
+          @num_shifts += 1
+        end 
+          i += 1
       end
-    # test if stored array == initial stored var
+      break if has_not_shifted
+      @num_of_iterations += 1
       i = 0
     end
-  puts arr
- 
+    puts "Number of iterations = #{@num_of_iterations}"
+      return arr
   end
-  
-# Need some validation to improve performance
-  def count_shifts
-    @shifted = true
 
+  def has_not_shifted
+    return true if @num_shifts == 0
   end
 
 end 
 
 
 my_var = Bubblesort.new()
-my_var.sort_array([4, 2, 9, 1, 5, 10, 2, 1])
-
+x = my_var.sort_array([9, 5, 3, 1, 9, 1, 7, 1])
+puts x
