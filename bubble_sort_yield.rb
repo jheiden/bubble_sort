0add3cf50afc645e050e473077557423007f1dca
Johan Heidenberg <jheidenberg@outlook.com>
2019-02-09 20:26:02 +0100

    completed the version using yield, added a switch statement solution

---
+++ bubble_sort_yield.rb
@@ -0,0 +1,48 @@
+
+# refactored version using a switch statement.
+
+def largest_num arr
+  temp = 0
+  (arr.length - 1).times do 
+  (arr.length - 1).times do |x|
+    test_output = yield(arr[x], arr[x + 1]) 
+    case test_output
+    when -1
+      arr[x + 1], arr[x] = arr[x], arr[x + 1]
+    when 1
+      arr[x], arr[x + 1] = arr[x + 1], arr[x]
+    end
+  end
+  end
+     arr
+end
+
+x = largest_num(["test", "hej", "aaaaaaaaaa", "m"]) do |left, right|
+  left.length <=> right.length
+  # end
+end
+
+
+
+def largest_num arr
+  temp = 0
+  
+  (arr.length - 1).times do 
+  (arr.length - 1).times do |x|
+    if(yield(arr[x], arr[x + 1]) > 0) 
+      arr[x], arr[x + 1] = arr[x + 1], arr[x]
+    end
+  end
+  end
+     arr
+end
+
+x = largest_num(["test", "hej", "aaaaaaaaaa"]) do |left, right|
+  left.length <=> right.length
+  # end
+end
+
+puts x
+
+   
+   
